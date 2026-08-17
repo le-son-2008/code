2e63a88f9c552f46d5035e2b670459ec49ccd7bb
Lê Thanh Sơn <lethankson2k8@gmail.com>
2026-08-17 11:09:09 +0700

    remake c

--- Csharp/Learn/If_else/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿bool honnayTroimua = true;
-if (honnayTroimua)
-{
-    Console.WriteLine("Hôm nay trời mưa");
-    Console.WriteLine("Mang theo ô nhé!");
-}
-else
-    Console.WriteLine("Hôm nay trời nắng");
--- Csharp/Learn/Value/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Xin chào mình là Lê Thanh Sơn");
-int age;
-age = 17;
-Console.WriteLine($"Mình năm nay {age} tuổi");
-bool isStudent = true;
-Console.WriteLine($"Mình là sinh viên: {isStudent}");
-char grade = 'A';
-Console.WriteLine($"Điểm của mình là: {grade}");
