7fa5237744a9aa56046ce477bbf832af048f46d5
Lê Thanh Sơn <lethankson2k8@gmail.com>
2026-09-13 20:24:18 +0700

    add cpp

---
+++ Cpp/Test/test2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+    cout<<"Xin chào các bạn";
+    return 0;
+}
